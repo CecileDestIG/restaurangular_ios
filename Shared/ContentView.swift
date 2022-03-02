a690d5f6d839d3dc553fe81ad0662f461aa69f48
Quentin Desbrousses <quentin.desbrousses@etu.umontpellier.fr>
2022-03-02 16:06:38 +0100

    update navbar

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,23 +12,27 @@ struct ContentView: View {
     var body: some View {
         TabView {
             Accueil().tabItem{
+                Image(systemName: "house.fill")
                 Text("Accueil")
             }.tag(0)
             RecetteListView().tabItem{
+                Image(systemName: "leaf.fill")
                 Text("Recettes")
             }.tag(1)
             IngredientListView().tabItem{
+                Image(systemName: "list.dash")
                 Text("Stock")
             }.tag(2)
+            AllergeneListView().tabItem{
+                Image(systemName: "heart.fill")
+                Text("Allergènes")
+            }.tag(5)
             CatIngrListView().tabItem{
                 Text("Cat. Ingrédient")
             }.tag(3)
             EtapeListView().tabItem{
                 Text("Etapes")
             }.tag(4)
-            AllergeneListView().tabItem{
-                Text("Allergènes")
-            }.tag(5)
         }
     }
 }
